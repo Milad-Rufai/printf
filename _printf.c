9c0b6c6996266d1c49b3fcc485853cfc25701c3c
Milad-Rufai <temilader19@gmail.com>
2022-10-19 02:14:45 -0700

    update

--- _printf.c
+++ _printf.c
@@ -1,12 +1,16 @@
 #include "main.h"
 
+
 void print_buffer(char buffer[], int *buff_ind);
+
+
 
 /**
  * _printf - Printf function
  * @format: format.
  * Return: Printed chars.
  */
+
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
@@ -14,10 +18,13 @@ int _printf(const char *format, ...)
 	va_list list;
 	char buffer[BUFF_SIZE];
 
+
 	if (format == NULL)
 		return (-1);
 
+
 	va_start(list, format);
+
 
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
@@ -26,9 +33,11 @@ int _printf(const char *format, ...)
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
+
 			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
+
 		else
 		{
 			print_buffer(buffer, &buff_ind);
@@ -45,20 +54,25 @@ int _printf(const char *format, ...)
 		}
 	}
 
+
 	print_buffer(buffer, &buff_ind);
-va_end(list);
+
+	va_end(list);
 
 	return (printed_chars);
 }
+
 
 /**
  * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
  */
+
 void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
 		write(1, &buffer[0], *buff_ind);
-*buff_ind = 0;
+
+	*buff_ind = 0;
 }
